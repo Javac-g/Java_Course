cf0ac8b85082c5684aa606c7e93febcce7afa0b7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-10-16 20:35:03 +0300

    Second

---
+++ Lecture2/Full/Cat.java
@@ -0,0 +1,7 @@
+package Lecture2.Full;
+
+public class Cat {
+    protected String color,name;
+    protected int age;
+
+}
---
+++ Lecture2/Parts/Eye.java
@@ -0,0 +1,10 @@
+package Lecture2.Parts;
+
+public class Eye {
+    private String  picture = "dreams";
+
+    public void see(String picture){
+        this.picture = picture;
+        System.out.println("I am  looking at " + picture + " now");
+    }
+}
