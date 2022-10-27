21f3e0584b2f1efa7e63c73a3efe324154cc3f96
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-10-27 23:30:24 +0300

    fix/super.

---
+++ Lecture6/Child.java
@@ -0,0 +1,17 @@
+package Lecture6;
+
+import java.io.IOException;
+
+public class Child extends Parent {
+    @Override
+    public Child reverse(long a) throws IOException{
+        return null;
+    }
+    public Child reverse(int b) throws IOException {
+        return null;
+    }
+    private Child reverse(long b,long a) throws Exception{
+        return null;
+    }
+
+}
---
+++ Lecture6/Parent.java
@@ -0,0 +1,9 @@
+package Lecture6;
+
+import java.io.IOException;
+
+public class Parent {
+    public Parent reverse(long a) throws Exception {
+        return null;
+    }
+}
