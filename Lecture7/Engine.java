feeac5d33bc6ebaeec3bd7793aca48bb17d009a5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-10-28 23:34:58 +0300

    V12, V8, Engine

---
+++ Lecture7/Engine.java
@@ -0,0 +1,10 @@
+package Lecture7;
+
+public class Engine {
+    protected String type ;
+
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ Lecture7/V12.java
@@ -0,0 +1,7 @@
+package Lecture7;
+
+public class V12 extends Engine {
+    V12(){
+        super.setType("V12");
+    }
+}
