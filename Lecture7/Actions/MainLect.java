060a06bc15971a85339d0acd6ecec94807bc2f72
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-01 22:00:47 +0200

    Lectors Version done

--- Lecture7/Actions/MainLect.java
+++ Lecture7/Actions/MainLect.java
@@ -2,6 +2,30 @@ package Lecture7.Actions;
 
 public class MainLect {
     public static void main(String...args){
+        UserLect user =  new UserLect();
+        int n = user.getRandomPult();
+
+        if(n == 0){
+            UniversalPultJapanise japanise = new UniversalPultJapanise();
+            japanise.turnOnAirCond();
+            japanise.turnOnSmartFridge();
+            japanise.turnOnWifiStove();
+            japanise.turnOnTv();
+        }else if(n == 1){
+            UniversalPultUkrain ukrain = new UniversalPultUkrain();
+            ukrain.turnOnAirCond();
+            ukrain.turnOnSmartFridge();
+            ukrain.turnOnWifiStove();
+            ukrain.turnOnTv();
+
+        } else if (n == 2) {
+            UniversalPultUSA usa = new UniversalPultUSA();
+            usa.turnOnAirCond();
+            usa.turnOnSmartFridge();
+            usa.turnOnWifiStove();
+            usaturnOnTv();
+
+        }
 
     }
 }
