b3220071e28888b751b7f07bd2c3a6604f571105
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-12 22:44:33 +0200

    Lection 10 - add client service

--- Lecture9/model/Tour.java
+++ Lecture9/model/Tour.java
@@ -7,11 +7,86 @@ import java.util.List;
 
 public class Tour {
     private Long number;
-    private COUNTRIES from,where;
+    private COUNTRIES from , where;
     private int cost;
-    private boolean nutrition,excursion;
+    private boolean nutrition , excursion;
     private byte daysAmount;
     private List<VACCINATIONS> vaccinations;
+
+    public Tour(Long number, COUNTRIES from, COUNTRIES where, int cost, boolean nutrition, boolean excursion, byte daysAmount, List<VACCINATIONS> vaccinations) {
+        this.number = number;
+        this.from = from;
+        this.where = where;
+        this.cost = cost;
+        this.nutrition = nutrition;
+        this.excursion = excursion;
+        this.daysAmount = daysAmount;
+        this.vaccinations = vaccinations;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public COUNTRIES getFrom() {
+        return from;
+    }
+
+    public void setFrom(COUNTRIES from) {
+        this.from = from;
+    }
+
+    public COUNTRIES getWhere() {
+        return where;
+    }
+
+    public void setWhere(COUNTRIES where) {
+        this.where = where;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public boolean isNutrition() {
+        return nutrition;
+    }
+
+    public void setNutrition(boolean nutrition) {
+        this.nutrition = nutrition;
+    }
+
+    public boolean isExcursion() {
+        return excursion;
+    }
+
+    public void setExcursion(boolean excursion) {
+        this.excursion = excursion;
+    }
+
+    public byte getDaysAmount() {
+        return daysAmount;
+    }
+
+    public void setDaysAmount(byte daysAmount) {
+        this.daysAmount = daysAmount;
+    }
+
+    public List<VACCINATIONS> getVaccinations() {
+        return vaccinations;
+    }
+
+    public void setVaccinations(List<VACCINATIONS> vaccinations) {
+        this.vaccinations = vaccinations;
+    }
 
     public int getComminCost(){
         return -1;
--- Lecture9/services/ClientService.java
+++ Lecture9/services/ClientService.java
@@ -3,14 +3,20 @@ package Lecture9.services;
 import Lecture9.model.Client;
 import Lecture9.model.enums.VACCINATIONS;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClientService {
+    private static final List<Client> clients = new ArrayList<>();
     public Client addClient(Long number, String firstName, String middleName,
                             String lastName, String ibanNumber,
                             List<VACCINATIONS> vaccinations){
 
-        return new Client(number,firstName,lastName,middleName,ibanNumber,vaccinations);
+        Client client = new Client(number,firstName,lastName,middleName,ibanNumber,vaccinations);
+        clients.add(client);
+        return client;
     }
+
+
 
 }
