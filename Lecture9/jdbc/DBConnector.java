9d89a055669cc80f3dd9db647b586dae804a138b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-20 22:45:33 +0200

    comm

--- Lecture9/jdbc/DBConnector.java
+++ Lecture9/jdbc/DBConnector.java
@@ -1,6 +1,10 @@
 package Lecture9.jdbc;
 
 public class DBConnector {
-    private static final String url = "jdbc:postgresql://localhost:5433"
+    private static final String url = "jdbc:postgresql://localhost:5433/touragency";
+    private static final String user = "postgres";
+    private static final String password = "postgres";
+    //Connection statement
+
 
 }
