b3fca49bee5269bea72283ff7b28d80570af2496
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-21 17:31:19 +0200

    connection

--- Lecture9/jdbc/DBConnector.java
+++ Lecture9/jdbc/DBConnector.java
@@ -1,10 +1,23 @@
 package Lecture9.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnector {
+
     private static final String url = "jdbc:postgresql://localhost:5433/touragency";
+
     private static final String user = "postgres";
+
     private static final String password = "postgres";
+
     //Connection statement
+    public static void main(String...args) throws SQLException {
+
+        Connection con = DriverManager.getConnection(url,user,password);
+        System.out.println(con);
+    }
 
 
 }
